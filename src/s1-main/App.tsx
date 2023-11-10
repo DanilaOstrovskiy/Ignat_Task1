374eaa31bd95021c089715157b57de641dfd2109
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-11-10 21:25:36 +0300

    -minor changes

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -9,19 +9,12 @@ import HW10 from "../s2-homeworks/hw10/HW10";
 import HW11 from "../s2-homeworks/hw11/HW11";
 import HW12 from "../s2-homeworks/hw12/HW12";
 import HW13 from "../s2-homeworks/hw13/HW13";
+import HW14 from "../s2-homeworks/hw14/HW14";
 
 function App() {
     return (
         <div className={s.App}>
             <HW5/>
-            <HW6/>
-            <HW7/>
-            <HW8/>
-            <HW9/>
-            <HW10/>
-            <HW11/>
-            <HW12/>
-            <HW13/>
         </div>
     )
 }
